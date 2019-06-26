b12ea3d33464212125ab0b47872d71198340ec2c
King Pra <kingi.pra@gmail.com>
2019-06-26 11:45:55 -0500

    adding comments to controller

--- src/main/java/com/kingpra/todoList/TodoListController.java
+++ src/main/java/com/kingpra/todoList/TodoListController.java
@@ -13,19 +13,27 @@ public class TodoListController {
 	@Autowired
 	private TodoRepository todoRepo;
 
+	// This is the initial method called.
+	// This method is also called if we are submitting a GET Request using a "/" at
+	// the end of the url.
 	@GetMapping({ "/" })
 	public String getIndexPage(Model model) {
+		// we retrieve all data (if any) from the database through the repository which
+		// gets sent to the model for our view.
 		model.addAttribute("todos", todoRepo.findAll());
 		return "index";
 	}
 
+	// this method is called when we make a GET Request using "/new" at the end of
+	// the url.
 	@GetMapping("/new")
 	public String addNewTodoPage() {
 		return "new";
 	}
 
 	@GetMapping("/edit/{id}")
-	public String getEditPage(@PathVariable Long id, Model model) {
+	public String getEditPage(Todo todo, @PathVariable Long id, Model model) {
+		model.addAttribute("todos", todoRepo.findAll());
 		model.addAttribute("todo", todoRepo.findById(id));
 		return "edit";
 	}
@@ -47,7 +55,8 @@ public class TodoListController {
 	@PostMapping("/edit/{id}")
 	public String editTask(Todo todo, @PathVariable Long id, Model model) {
 		Todo task = todoRepo.findById(id).orElse(null);
-		task.setTask(task.getTask());
+		task.setTask(todo.getTask());
+		todoRepo.save(task);
 		model.addAttribute("todos", todoRepo.findAll());
 		return "index";
 	}
